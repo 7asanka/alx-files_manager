ca981cc9896d8027ca83e924c2cd3c49b0915584
Hassan Ibrahim <hassan2002_kimo@hotmail.com>
2025-06-22 13:07:33 -0700

    added lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,5 +33,9 @@ module.exports = {
         files: ['*.js'],
         excludedFiles: 'babel.config.js',
       }
-    ]
+    ],
+    rules: {
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+    }
 };
